36aa8edad9dabadcb546c26bdb37dccbad2929fa
Cyberduc-k <tttymo@gmail.com>
2019-10-08 09:46:08 +0200

    opdracht 4

---
+++ Arrays/Opdracht 4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] getallen = new int[20];
+
+            for (int i = 0; i < getallen.Length; i++)
+            {
+                Console.Write("Geef een getal: ");
+                int getal = int.Parse(Console.ReadLine());
+
+                if (getal == 0) break;
+
+                getallen[i] = getal;
+            }
+
+            Console.Write("Geef de zoekwaarde: ");
+            int zoek = int.Parse(Console.ReadLine());
+            int aantal = 0;
+
+            foreach (int getal in getallen)
+            {
+                if (getal == zoek) aantal++;
+            }
+
+            Console.WriteLine($"De zoekwaarde komt {aantal} keer voor");
+            Console.ReadKey();
+        }
+    }
+}
