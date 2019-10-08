11cf84edb9e80505bce8d22da741668ae3804c27
Cyberduc-k <tttymo@gmail.com>
2019-10-08 11:44:45 +0200

    opdtacht 7

---
+++ Arrays/Opdracht 7/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Opdracht_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelWaarde1 = new System.Windows.Forms.Label();
+            this.labelWaarde2 = new System.Windows.Forms.Label();
+            this.labelWaarde3 = new System.Windows.Forms.Label();
+            this.labelWaarde4 = new System.Windows.Forms.Label();
+            this.labelWaarde5 = new System.Windows.Forms.Label();
+            this.labelWaarde6 = new System.Windows.Forms.Label();
+            this.buttonGooien = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelWaarde1
+            // 
+            this.labelWaarde1.AutoSize = true;
+            this.labelWaarde1.Location = new System.Drawing.Point(13, 13);
+            this.labelWaarde1.Name = "labelWaarde1";
+            this.labelWaarde1.Size = new System.Drawing.Size(138, 13);
+            this.labelWaarde1.TabIndex = 0;
+            this.labelWaarde1.Text = "Waarde 1 is 0 keer gegooid";
+            // 
+            // labelWaarde2
+            // 
+            this.labelWaarde2.AutoSize = true;
+            this.labelWaarde2.Location = new System.Drawing.Point(13, 35);
+            this.labelWaarde2.Name = "labelWaarde2";
+            this.labelWaarde2.Size = new System.Drawing.Size(138, 13);
+            this.labelWaarde2.TabIndex = 1;
+            this.labelWaarde2.Text = "Waarde 2 is 0 keer gegooid";
+            // 
+            // labelWaarde3
+            // 
+            this.labelWaarde3.AutoSize = true;
+            this.labelWaarde3.Location = new System.Drawing.Point(12, 58);
+            this.labelWaarde3.Name = "labelWaarde3";
+            this.labelWaarde3.Size = new System.Drawing.Size(138, 13);
+            this.labelWaarde3.TabIndex = 2;
+            this.labelWaarde3.Text = "Waarde 3 is 0 keer gegooid";
+            // 
+            // labelWaarde4
+            // 
+            this.labelWaarde4.AutoSize = true;
+            this.labelWaarde4.Location = new System.Drawing.Point(13, 81);
+            this.labelWaarde4.Name = "labelWaarde4";
+            this.labelWaarde4.Size = new System.Drawing.Size(138, 13);
+            this.labelWaarde4.TabIndex = 3;
+            this.labelWaarde4.Text = "Waarde 4 is 0 keer gegooid";
+            // 
+            // labelWaarde5
+            // 
+            this.labelWaarde5.AutoSize = true;
+            this.labelWaarde5.Location = new System.Drawing.Point(13, 103);
+            this.labelWaarde5.Name = "labelWaarde5";
+            this.labelWaarde5.Size = new System.Drawing.Size(138, 13);
+            this.labelWaarde5.TabIndex = 4;
+            this.labelWaarde5.Text = "Waarde 5 is 0 keer gegooid";
+            // 
+            // labelWaarde6
+            // 
+            this.labelWaarde6.AutoSize = true;
+            this.labelWaarde6.Location = new System.Drawing.Point(13, 126);
+            this.labelWaarde6.Name = "labelWaarde6";
+            this.labelWaarde6.Size = new System.Drawing.Size(138, 13);
+            this.labelWaarde6.TabIndex = 5;
+            this.labelWaarde6.Text = "Waarde 6 is 0 keer gegooid";
+            // 
+            // buttonGooien
+            // 
+            this.buttonGooien.Location = new System.Drawing.Point(12, 153);
+            this.buttonGooien.Name = "buttonGooien";
+            this.buttonGooien.Size = new System.Drawing.Size(167, 23);
+            this.buttonGooien.TabIndex = 6;
+            this.buttonGooien.Text = "Gooien";
+            this.buttonGooien.UseVisualStyleBackColor = true;
+            this.buttonGooien.Click += new System.EventHandler(this.ButtonGooien_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(192, 188);
+            this.Controls.Add(this.buttonGooien);
+            this.Controls.Add(this.labelWaarde6);
+            this.Controls.Add(this.labelWaarde5);
+            this.Controls.Add(this.labelWaarde4);
+            this.Controls.Add(this.labelWaarde3);
+            this.Controls.Add(this.labelWaarde2);
+            this.Controls.Add(this.labelWaarde1);
+            this.Name = "Form1";
+            this.Text = "Opdracht 7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWaarde1;
+        private System.Windows.Forms.Label labelWaarde2;
+        private System.Windows.Forms.Label labelWaarde3;
+        private System.Windows.Forms.Label labelWaarde4;
+        private System.Windows.Forms.Label labelWaarde5;
+        private System.Windows.Forms.Label labelWaarde6;
+        private System.Windows.Forms.Button buttonGooien;
+    }
+}
+
---
+++ Arrays/Opdracht 7/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opdracht_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonGooien_Click(object sender, EventArgs e)
+        {
+            int[] dice = new int[6];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 6000; i++)
+            {
+                int value = rnd.Next(1, 7);
+
+                dice[value - 1]++;
+            }
+
+            labelWaarde1.Text = $"Waarde 1 is {dice[0]} keer gegooid";
+            labelWaarde2.Text = $"Waarde 2 is {dice[1]} keer gegooid";
+            labelWaarde3.Text = $"Waarde 3 is {dice[2]} keer gegooid";
+            labelWaarde4.Text = $"Waarde 4 is {dice[3]} keer gegooid";
+            labelWaarde5.Text = $"Waarde 5 is {dice[4]} keer gegooid";
+            labelWaarde6.Text = $"Waarde 6 is {dice[5]} keer gegooid";
+        }
+    }
+}
