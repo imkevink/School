8889670b44cac21caa833ec98545c10ab5838fbb
Cyberduc-k <tttymo@gmail.com>
2019-10-09 14:20:48 +0200

    opdracht 1

---
+++ Methoden/Opdracht 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef de prijs: ");
+            float prijs = float.Parse(Console.ReadLine());
+            float btw = BerekenBtw(prijs);
+            float totaal = prijs + btw;
+
+            Console.WriteLine($"Prijs: {prijs}, btw: {btw}, totaal: {totaal}");
+            Console.ReadKey();
+        }
+
+        static float BerekenBtw(float prijs)
+        {
+            const float BTW = 0.21f;
+
+            return prijs * BTW;
+        }
+    }
+}
